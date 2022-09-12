f89263584348b380dcbaac67284bcc0725c7a15a
ewauq <emilien.wauquier@gmail.com>
2022-09-12 21:57:04 +0200

    Added the script base files

---
+++ rym-visual-rating/src/userscript.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         RateYourMusic - Visual Rating
+// @namespace    rym-visual-rating
+// @version      0.4
+// @description  Adds a simple horizontal and visual rating on releases pages.
+// @author       Kazaam
+// @match        https://rateyourmusic.com/release/*
+// @icon         https://www.google.com/s2/favicons?domain=rateyourmusic.com
+// ==/UserScript==
+/* eslint-disable @typescript-eslint/no-extra-semi */
+;(function () {
+  ;('use strict')
+  console.log('Hello')
+})()
+//# sourceMappingURL=userscript.js.map
