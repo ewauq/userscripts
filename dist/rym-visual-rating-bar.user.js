1ecd536919ba7b6695663387286b0a19e60fb31a
ewauq <emilien.wauquier@gmail.com>
2022-09-14 22:32:45 +0200

    Updated project structure

--- dist/rym-visual-rating-bar.user.js
+++ dist/rym-visual-rating-bar.user.js
@@ -1,107 +1,84 @@
 // ==UserScript==
-// @name         RateYourMusic - Visual Rating
-// @namespace    rym-visual-rating
+// @name         RateYourMusic - Visual Rating Bar
+// @namespace    rym-visual-rating-bar
 // @version      0.4
-// @description  Adds a simple horizontal and visual rating bar on releases pages.
+// @description  Adds a visual rating bar on releases pages.
 // @author       ewauq
-// @updateURL    https://raw.githubusercontent.com/ewauq/userscripts/main/rym-visual-rating/src/script.user.js
-// @downloadURL  https://raw.githubusercontent.com/ewauq/userscripts/main/rym-visual-rating/src/script.user.js
+// @updateURL    https://raw.githubusercontent.com/ewauq/userscripts/main/dist/rym-visual-rating-bar.user.js
+// @downloadURL  https://raw.githubusercontent.com/ewauq/userscripts/main/dist/rym-visual-rating-bar.user.js
 // @supportURL   https://github.com/ewauq/userscripts/issues
 // @match        https://rateyourmusic.com/release/*
 // @icon         https://www.google.com/s2/favicons?domain=rateyourmusic.com
 // ==/UserScript==
-
-type BarOptions = {
-  animation?: boolean
-  borderRadius?: number | false
-  colors?: 'vibrant' | 'rainbow' | 'neon' | 'colorBlind'
-  themeMode?: string | false
-  height?: number
-  shadow?: boolean
-  style?: 'blend' | 'gradual'
-}
-
-type RGBColor = `rgb(${number}, ${number}, ${number})`
-type ThemeColors = { [themeName: string]: `#${string}`[] }
-
-const Themes: ThemeColors = {
+const Themes = {
   vibrant: ['#fa4146', '#fa961e', '#fac850', '#91be6e', '#55a569', '#4182a5'],
   rainbow: ['#ff0000', '#ff961e', '#ffff00', '#00cd00', '#aaaaff', '#6532ff', '#c800ff'],
   neon: ['#ff00d9', '#ffb400', '#96ff00', '#00ffc8', '#0082ff', '#b432ff'],
   colorBlind: ['#dc321e', '#ffb400', '#faff00', '#23fffa', '#28b4ff', '#2850ff'],
 }
-
 class VisualRatingBar {
-  barOptions: BarOptions = {}
-
-  constructor(options?: BarOptions) {
-    this.barOptions.animation = options?.animation === undefined || options?.animation
-    this.barOptions.borderRadius = options?.borderRadius || 6
-    this.barOptions.colors = options?.colors || 'vibrant'
+  constructor(options) {
+    this.barOptions = {}
+    this.getBackgroundColor = () => {
+      const elementNode = document.querySelector('.release_right_column')
+      return window.getComputedStyle(elementNode).backgroundColor
+    }
+    this.getThemeMode = () => {
+      const currentThemeMode = localStorage.getItem('theme')
+      if (!['eve', 'night', 'light'].includes(currentThemeMode)) return
+      return currentThemeMode
+    }
+    this.buildGradient = () => {
+      const { colors, style } = this.barOptions
+      const colorsCount = colors.length - 1
+      const colorCodes = Themes[colors]
+      const gradientStep = 100 / colorsCount
+      const cssValues = []
+      let currentStep = 0
+      colorCodes.forEach((color, index) => {
+        cssValues.push(`${color} ${currentStep}%`)
+        if (style === 'gradual' && index + 1 < colorsCount) {
+          cssValues.push(`${color} ${currentStep + gradientStep}%`)
+        }
+        // Needed to prevent artifacts from elements superposition with a border radius
+        if (index + 1 == colorsCount) {
+          cssValues.push(`${color} 98%`)
+          cssValues.push('transparent 98%')
+          cssValues.push('transparent 100%')
+        }
+        currentStep += gradientStep
+      })
+      return cssValues.join(', ')
+    }
+    this.barOptions.animation =
+      (options === null || options === void 0 ? void 0 : options.animation) === undefined ||
+      (options === null || options === void 0 ? void 0 : options.animation)
+    this.barOptions.borderRadius =
+      (options === null || options === void 0 ? void 0 : options.borderRadius) || 6
+    this.barOptions.colors =
+      (options === null || options === void 0 ? void 0 : options.colors) || 'vibrant'
     this.barOptions.themeMode = this.getThemeMode() || false
-    this.barOptions.height = options?.height || 20
-    this.barOptions.shadow = options?.shadow === undefined || options?.shadow
-    this.barOptions.style = options?.style || 'gradual'
+    this.barOptions.height =
+      (options === null || options === void 0 ? void 0 : options.height) || 20
+    this.barOptions.shadow =
+      (options === null || options === void 0 ? void 0 : options.shadow) === undefined ||
+      (options === null || options === void 0 ? void 0 : options.shadow)
+    this.barOptions.style =
+      (options === null || options === void 0 ? void 0 : options.style) || 'gradual'
   }
-
-  getBackgroundColor = (): RGBColor => {
-    const elementNode: Element = document.querySelector('.release_right_column')
-    return window.getComputedStyle(elementNode).backgroundColor as RGBColor
-  }
-
-  getThemeMode = (): string | null => {
-    const currentThemeMode = localStorage.getItem('theme')
-    if (!['eve', 'night', 'light'].includes(currentThemeMode)) return
-    return currentThemeMode
-  }
-
-  buildGradient = (): string => {
-    const { colors, style } = this.barOptions
-
-    const colorsCount = colors.length - 1
-    const colorCodes = Themes[colors]
-    const gradientStep: number = 100 / colorsCount
-
-    const cssValues: string[] = []
-    let currentStep = 0
-
-    colorCodes.forEach((color, index) => {
-      cssValues.push(`${color} ${currentStep}%`)
-
-      if (style === 'gradual' && index + 1 < colorsCount) {
-        cssValues.push(`${color} ${currentStep + gradientStep}%`)
-      }
-
-      // Needed to prevent artifacts from elements superposition with a border radius
-      if (index + 1 == colorsCount) {
-        cssValues.push(`${color} 98%`)
-        cssValues.push('transparent 98%')
-        cssValues.push('transparent 100%')
-      }
-
-      currentStep += gradientStep
-    })
-
-    return cssValues.join(', ')
-  }
-
-  init(): void {
-    const ratingNode: Element = document.querySelector('span.avg_rating')
+  init() {
+    const ratingNode = document.querySelector('span.avg_rating')
     if (!ratingNode) return
-
     const { animation, height, borderRadius, themeMode: mode, shadow } = this.barOptions
-
-    const rating: number = parseFloat(ratingNode.textContent.trim())
-    const ratingPercentage: number = (rating * 100) / 5
-
+    const rating = parseFloat(ratingNode.textContent.trim())
+    const ratingPercentage = (rating * 100) / 5
     // Bar wrapper
-    const barWrapper: HTMLDivElement = document.createElement('div')
+    const barWrapper = document.createElement('div')
     barWrapper.id = 'userscript-bar-wrapper'
     barWrapper.style.height = `${height}px`
     barWrapper.style.position = 'relative'
     barWrapper.style.marginTop = '6px'
     barWrapper.title = `${rating}/5 (${ratingPercentage}%)`
-
     // Bar mask
     const barMask = document.createElement('div')
     barMask.style.width = animation ? '90%' : `${100 - ratingPercentage}%`
@@ -115,20 +92,17 @@ class VisualRatingBar {
     if (borderRadius) barMask.style.borderTopRightRadius = `${borderRadius}px`
     if (borderRadius) barMask.style.borderBottomRightRadius = `${borderRadius}px`
     if (shadow) barMask.style.boxShadow = '#00000063 0px 0px 3px 0px inset'
-
     // Bar gradient
-    const barGradient: HTMLDivElement = document.createElement('div')
+    const barGradient = document.createElement('div')
     barGradient.style.height = '100%'
     barGradient.style.background = `linear-gradient(90deg, ${this.buildGradient()})`
     if (borderRadius) barGradient.style.borderRadius = `${borderRadius}px`
     if (mode !== 'light') barGradient.style.filter = 'saturate(50%)'
     if (shadow) barGradient.style.boxShadow = '#0000009c 0px 0px 4px 0px inset'
-
     const ratingNodeParent = ratingNode.parentNode.parentNode
     ratingNodeParent.appendChild(barWrapper)
     barWrapper.appendChild(barGradient)
     barWrapper.appendChild(barMask)
-
     // Bar animation
     const waitingBarVisiblility = window.setInterval(function () {
       const barWrapperNode = document.getElementById('userscript-bar-wrapper')
@@ -137,16 +111,13 @@ class VisualRatingBar {
         window.clearInterval(waitingBarVisiblility)
       }
     }, 50)
-
     // Handling RYM theme mode switching
     document.querySelectorAll('div.header_theme_button')[1].addEventListener('click', () => {
       const themeMode = this.getThemeMode()
       barMask.style.backgroundColor = this.getBackgroundColor()
       barGradient.style.filter = `saturate(${themeMode === 'light' ? 100 : 50}%)`
     })
-
     console.log('[USERSCRIPT] RateYourMusic Visual Rating Bar added')
   }
 }
-
 new VisualRatingBar().init()
---
+++ src/rateyourmusic/type/BarOptions.ts
@@ -0,0 +1,9 @@
+export type BarOptions = {
+  animation?: boolean
+  borderRadius?: number | false
+  colors?: 'vibrant' | 'rainbow' | 'neon' | 'colorBlind'
+  themeMode?: string | false
+  height?: number
+  shadow?: boolean
+  style?: 'blend' | 'gradual'
+}
